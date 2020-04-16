826095438e06254307321ffdc1a86b0e91344c78
Marcos Canales <mlcanales@uc.cl>
2020-04-16 16:41:31 -0400

    Add files via upload

---
+++ js/zotero-tree-data.js
@@ -0,0 +1,545 @@
+var tree =
+[
+  {
+    "text": "Metodologias",
+    "item-id": "c60,iNone",
+    "nodes": [
+      {
+        "text": "Participatory Modelling",
+        "item-id": "c63,i3002",
+        "nodes": [
+          {
+            "text": "Making the most of mental models",
+            "item-id": "i3002",
+            "nodes": [
+              {
+                "text": "LaMere et al. - 2020 - Making the most of mental models Advancing the me.pdf",
+                "item-id": "i3000",
+                "icon": "glyphicon glyphicon-paperclip",
+                "item_title": "LaMere et al. - 2020 - Making the most of mental models Advancing the me.pdf",
+                "item_type": "attachment",
+                "item_note": null,
+                "node_type": "item",
+                "metadata": [
+                  [
+                    "Title",
+                    "LaMere et al. - 2020 - Making the most of mental models Advancing the me.pdf"
+                  ]
+                ],
+                "resource": "storage/i3000.pdf"
+              }
+            ],
+            "icon": "glyphicon glyphicon-file",
+            "item_title": "Making the most of mental models",
+            "item_type": "journalArticle",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Abstract Note",
+                "Eliciting stakeholders\u2019 mental models is an important participatory modeling (PM) tool for building systems knowledge, a frequent challenge in natural resource management. Therefore, mental models constitute a valu\u00ad able source of information, making it imperative to document them in detail, while preserving the integrity of stakeholders\u2019 beliefs. We propose a methodology, the Rich Elicitation Approach (REA), which combines direct and indirect elicitation techniques to meet these goals. We describe the approach in the context of the effects of climate change on Baltic salmon. The REA produced holistic depictions of mental models, with more variables and causal relationships per diagram than direct elicitation alone, thus providing a solid knowledge base on which to begin PM studies. The REA was well received by stakeholders and fulfilled the substantive, normative, instrumental, and educational functions of PM. However, motivating stakeholders to confirm the accuracy of their models during the verification stage of the REA was challenging."
+              ],
+              [
+                "Access Date",
+                "2020-04-16 20:19:43"
+              ],
+              [
+                "Creators",
+                "Kelsey LaMere, Samu M\u00e4ntyniemi, Jarno Vanhatalo, P\u00e4ivi Haapasaari"
+              ],
+              [
+                "DOI",
+                "10/gghw9p"
+              ],
+              [
+                "Date",
+                "2020-02-00 02/2020"
+              ],
+              [
+                "ISSN",
+                "13648152"
+              ],
+              [
+                "Journal Abbreviation",
+                "Environmental Modelling & Software"
+              ],
+              [
+                "Language",
+                "en"
+              ],
+              [
+                "Library Catalog",
+                "DOI.org (Crossref)"
+              ],
+              [
+                "Pages",
+                "104589"
+              ],
+              [
+                "Publication Title",
+                "Environmental Modelling & Software"
+              ],
+              [
+                "Short Title",
+                "Making the most of mental models"
+              ],
+              [
+                "Title",
+                "Making the most of mental models: Advancing the methodology for mental model elicitation and documentation with expert stakeholders"
+              ],
+              [
+                "URL",
+                "https://linkinghub.elsevier.com/retrieve/pii/S1364815219301495"
+              ],
+              [
+                "Volume",
+                "124"
+              ]
+            ],
+            "resource": "storage/i3000.pdf",
+            "selectable": false
+          },
+          {
+            "text": "Wisdom of stakeholder crowds in complex social\u2013ecological systems",
+            "item-id": "i3001",
+            "nodes": [
+              {
+                "text": "Aminpour et al. - 2020 - Wisdom of stakeholder crowds in complex social\u2013eco.pdf",
+                "item-id": "i2999",
+                "icon": "glyphicon glyphicon-paperclip",
+                "item_title": "Aminpour et al. - 2020 - Wisdom of stakeholder crowds in complex social\u2013eco.pdf",
+                "item_type": "attachment",
+                "item_note": null,
+                "node_type": "item",
+                "metadata": [
+                  [
+                    "Title",
+                    "Aminpour et al. - 2020 - Wisdom of stakeholder crowds in complex social\u2013eco.pdf"
+                  ]
+                ],
+                "resource": "storage/i2999.pdf"
+              }
+            ],
+            "icon": "glyphicon glyphicon-file",
+            "item_title": "Wisdom of stakeholder crowds in complex social\u2013ecological systems",
+            "item_type": "journalArticle",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2020-04-16 20:19:41"
+              ],
+              [
+                "Creators",
+                "Payam Aminpour, Steven A. Gray, Antonie J. Jetter, Joshua E. Introne, Alison Singer, Robert Arlinghaus"
+              ],
+              [
+                "DOI",
+                "10/ggr72q"
+              ],
+              [
+                "Date",
+                "2020-03-00 3/2020"
+              ],
+              [
+                "ISSN",
+                "2398-9629"
+              ],
+              [
+                "Issue",
+                "3"
+              ],
+              [
+                "Journal Abbreviation",
+                "Nat Sustain"
+              ],
+              [
+                "Language",
+                "en"
+              ],
+              [
+                "Library Catalog",
+                "DOI.org (Crossref)"
+              ],
+              [
+                "Pages",
+                "191-199"
+              ],
+              [
+                "Publication Title",
+                "Nature Sustainability"
+              ],
+              [
+                "Title",
+                "Wisdom of stakeholder crowds in complex social\u2013ecological systems"
+              ],
+              [
+                "URL",
+                "http://www.nature.com/articles/s41893-019-0467-z"
+              ],
+              [
+                "Volume",
+                "3"
+              ]
+            ],
+            "resource": "storage/i2999.pdf",
+            "selectable": false
+          }
+        ],
+        "item_title": "Participatory Modelling",
+        "item_type": null,
+        "item_note": null,
+        "node_type": "collection",
+        "selectable": false
+      },
+      {
+        "text": "Socio-Hydrology",
+        "item-id": "c62,i2996",
+        "nodes": [
+          {
+            "text": "Social tipping points in global groundwater management",
+            "item-id": "i2996",
+            "nodes": [
+              {
+                "text": "Castilla-Rho et al. - 2017 - Social tipping points in global groundwater manage.pdf",
+                "item-id": "i2995",
+                "icon": "glyphicon glyphicon-paperclip",
+                "item_title": "Castilla-Rho et al. - 2017 - Social tipping points in global groundwater manage.pdf",
+                "item_type": "attachment",
+                "item_note": null,
+                "node_type": "item",
+                "metadata": [
+                  [
+                    "Title",
+                    "Castilla-Rho et al. - 2017 - Social tipping points in global groundwater manage.pdf"
+                  ]
+                ],
+                "resource": "storage/i2995.pdf"
+              }
+            ],
+            "icon": "glyphicon glyphicon-file",
+            "item_title": "Social tipping points in global groundwater management",
+            "item_type": "journalArticle",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2020-04-16 20:18:29"
+              ],
+              [
+                "Creators",
+                "Juan Carlos Castilla-Rho, Rodrigo Rojas, Martin S. Andersen, Cameron Holley, Gregoire Mariethoz"
+              ],
+              [
+                "DOI",
+                "10/gc3gkn"
+              ],
+              [
+                "Date",
+                "2017-09-00 9/2017"
+              ],
+              [
+                "ISSN",
+                "2397-3374"
+              ],
+              [
+                "Issue",
+                "9"
+              ],
+              [
+                "Journal Abbreviation",
+                "Nat Hum Behav"
+              ],
+              [
+                "Language",
+                "en"
+              ],
+              [
+                "Library Catalog",
+                "DOI.org (Crossref)"
+              ],
+              [
+                "Pages",
+                "640-649"
+              ],
+              [
+                "Publication Title",
+                "Nature Human Behaviour"
+              ],
+              [
+                "Title",
+                "Social tipping points in global groundwater management"
+              ],
+              [
+                "URL",
+                "http://www.nature.com/articles/s41562-017-0181-7"
+              ],
+              [
+                "Volume",
+                "1"
+              ]
+            ],
+            "resource": "storage/i2995.pdf",
+            "selectable": false
+          },
+          {
+            "text": "Socio-hydrology",
+            "item-id": "i2994",
+            "nodes": [
+              {
+                "text": "Sivapalan et al. - 2012 - Socio-hydrology A new science of people and water.pdf",
+                "item-id": "i2993",
+                "icon": "glyphicon glyphicon-paperclip",
+                "item_title": "Sivapalan et al. - 2012 - Socio-hydrology A new science of people and water.pdf",
+                "item_type": "attachment",
+                "item_note": null,
+                "node_type": "item",
+                "metadata": [
+                  [
+                    "Title",
+                    "Sivapalan et al. - 2012 - Socio-hydrology A new science of people and water.pdf"
+                  ]
+                ],
+                "resource": "storage/i2993.pdf"
+              }
+            ],
+            "icon": "glyphicon glyphicon-file",
+            "item_title": "Socio-hydrology",
+            "item_type": "journalArticle",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2020-04-16 20:18:00"
+              ],
+              [
+                "Creators",
+                "Murugesu Sivapalan, Hubert H. G. Savenije, G\u00fcnter Bl\u00f6schl"
+              ],
+              [
+                "DOI",
+                "10/b3hp2w"
+              ],
+              [
+                "Date",
+                "2012-04-15 2012-04-15"
+              ],
+              [
+                "ISSN",
+                "08856087"
+              ],
+              [
+                "Issue",
+                "8"
+              ],
+              [
+                "Journal Abbreviation",
+                "Hydrol. Process."
+              ],
+              [
+                "Language",
+                "en"
+              ],
+              [
+                "Library Catalog",
+                "DOI.org (Crossref)"
+              ],
+              [
+                "Pages",
+                "1270-1276"
+              ],
+              [
+                "Publication Title",
+                "Hydrological Processes"
+              ],
+              [
+                "Short Title",
+                "Socio-hydrology"
+              ],
+              [
+                "Title",
+                "Socio-hydrology: A new science of people and water: INVITED COMMENTARY"
+              ],
+              [
+                "URL",
+                "http://doi.wiley.com/10.1002/hyp.8426"
+              ],
+              [
+                "Volume",
+                "26"
+              ]
+            ],
+            "resource": "storage/i2993.pdf",
+            "selectable": false
+          },
+          {
+            "text": "Socio-hydrological modelling: a review asking \u201cwhy, what and how?\u201d",
+            "item-id": "i2992",
+            "nodes": [
+              {
+                "text": "Blair y Buytaert - 2016 - Socio-hydrological modelling a review asking \u201cwhy.pdf",
+                "item-id": "i2991",
+                "icon": "glyphicon glyphicon-paperclip",
+                "item_title": "Blair y Buytaert - 2016 - Socio-hydrological modelling a review asking \u201cwhy.pdf",
+                "item_type": "attachment",
+                "item_note": null,
+                "node_type": "item",
+                "metadata": [
+                  [
+                    "Title",
+                    "Blair y Buytaert - 2016 - Socio-hydrological modelling a review asking \u201cwhy.pdf"
+                  ]
+                ],
+                "resource": "storage/i2991.pdf"
+              }
+            ],
+            "icon": "glyphicon glyphicon-file",
+            "item_title": "Socio-hydrological modelling: a review asking \u201cwhy, what and how?\u201d",
+            "item_type": "journalArticle",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Creators",
+                "P Blair, W Buytaert"
+              ],
+              [
+                "DOI",
+                "10/f79v8v"
+              ],
+              [
+                "Date",
+                "2016-00-00 2016"
+              ],
+              [
+                "Language",
+                "en"
+              ],
+              [
+                "Library Catalog",
+                "Zotero"
+              ],
+              [
+                "Pages",
+                "36"
+              ],
+              [
+                "Publication Title",
+                "Hydrol. Earth Syst. Sci."
+              ],
+              [
+                "Title",
+                "Socio-hydrological modelling: a review asking \u201cwhy, what and how?\u201d"
+              ]
+            ],
+            "resource": "storage/i2991.pdf",
+            "selectable": false
+          }
+        ],
+        "item_title": "Socio-Hydrology",
+        "item_type": null,
+        "item_note": null,
+        "node_type": "collection",
+        "selectable": false
+      },
+      {
+        "text": "Exploratory Modelling",
+        "item-id": "c61,i2998",
+        "nodes": [
+          {
+            "text": "Decision Making under Deep Uncertainty",
+            "item-id": "i2998",
+            "nodes": [
+              {
+                "text": "Marchau et al. - 2019 - Decision Making under Deep Uncertainty From Theor.pdf",
+                "item-id": "i2997",
+                "icon": "glyphicon glyphicon-paperclip",
+                "item_title": "Marchau et al. - 2019 - Decision Making under Deep Uncertainty From Theor.pdf",
+                "item_type": "attachment",
+                "item_note": null,
+                "node_type": "item",
+                "metadata": [
+                  [
+                    "Title",
+                    "Marchau et al. - 2019 - Decision Making under Deep Uncertainty From Theor.pdf"
+                  ]
+                ],
+                "resource": "storage/i2997.pdf"
+              }
+            ],
+            "icon": "glyphicon glyphicon-book",
+            "item_title": "Decision Making under Deep Uncertainty",
+            "item_type": "book",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2020-04-16 20:18:40"
+              ],
+              [
+                "Creators",
+                "Vincent A. W. J. Marchau, Warren E. Walker, Pieter J. T. M. Bloemen, Steven W. Popper"
+              ],
+              [
+                "Date",
+                "2019-00-00 2019"
+              ],
+              [
+                "Extra",
+                "DOI: 10.1007/978-3-030-05252-2"
+              ],
+              [
+                "ISBN",
+                "978-3-030-05251-5 978-3-030-05252-2"
+              ],
+              [
+                "Language",
+                "en"
+              ],
+              [
+                "Library Catalog",
+                "DOI.org (Crossref)"
+              ],
+              [
+                "Place",
+                "Cham"
+              ],
+              [
+                "Publisher",
+                "Springer International Publishing"
+              ],
+              [
+                "Short Title",
+                "Decision Making under Deep Uncertainty"
+              ],
+              [
+                "Title",
+                "Decision Making under Deep Uncertainty: From Theory to Practice"
+              ],
+              [
+                "URL",
+                "http://link.springer.com/10.1007/978-3-030-05252-2"
+              ]
+            ],
+            "resource": "storage/i2997.pdf",
+            "selectable": false
+          }
+        ],
+        "item_title": "Exploratory Modelling",
+        "item_type": null,
+        "item_note": null,
+        "node_type": "collection",
+        "selectable": false
+      }
+    ],
+    "item_title": "Metodologias",
+    "item_type": null,
+    "item_note": null,
+    "node_type": "collection",
+    "selectable": false
+  }
+]
